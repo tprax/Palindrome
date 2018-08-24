5230f686a0721b7c52cf51289d079e9e99b66204
tprax <tprax01@gmail.com>
2018-08-24 09:43:10 -0600

    first update

---
+++ main.rb
@@ -0,0 +1,23 @@
+# greet
+# get word
+# test word
+# display results
+
+def greet
+  puts "Welcome lets check to see if your word is a Palindrome"
+  word
+end
+
+def word
+  puts 'What word would you like to test?'
+  @users_word = gets.strip
+end
+
+def test
+
+end
+
+def results
+
+end
+greet
